f2effcaeb1b49d347eb8f74334b3f2c69d7eaa78
Ulyana Yunusova <85567113+Ulyanaaa@users.noreply.github.com>
2023-04-26 10:04:33 +0300

    Add files via upload

---
+++ kursovaya/header.h
@@ -0,0 +1,66 @@
+/*
+70
+Цветочная фирма «Красная лилия» заказала программу обработки информации об
+однолетних цветах. О каждом сорте летника в файл записываются следующие данные:
+название сорта, цветовая гамма, высота взрослого растения, время в днях от дня посадки
+до начала цветения, продолжительность цветения. Программа составляет список сортов,
+которые, будучи высажены в день запроса, обеспечивают цветение в заданный временной
+интервал (дата начала цветения, и дата конца цветения задаются с клавиатуры). Сорта в
+списке расположить в порядке уменьшения высоты растений.
+*/
+
+// Библиотеки
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <cstdlib> // Для функции exit()
+#include <cstring>
+#include <string.h>
+#include <algorithm>
+#include <iomanip>
+#include <windows.h>
+
+using namespace std;
+
+extern int len;
+extern const char* fname;
+
+// Структура магазина
+struct Shop {
+    char name[20];
+    char color[20];
+    int hight;
+    int grow;
+    int bloom;
+    
+    int start_bloom[6];
+    int end_bloom[6];
+
+    char marka[2];
+};
+
+
+// Заголовок(шапка) иразделяющая линия
+ostream& shapka(ostream& stream);
+ostream& dividing_line(ostream& stream);
+
+
+
+//Прототипы
+int menu();
+
+void write_new(fstream& p);
+void vivod(fstream& p);
+void print(Shop zap);
+void print_res_bloom(fstream& p, int start_bloom[], int end_bloom[]);
+int perevod_dat(int array[]);
+void write(fstream& p, Shop zap, int num = 0);
+
+fstream& read(fstream& p, Shop& zap, int num = 0);
+Shop vvod(fstream& p);
+
+int metka_for_delete(fstream& p, char* sort);
+void delete_marks(fstream& p);
+int delet_who_with_mark(fstream& p);
+
